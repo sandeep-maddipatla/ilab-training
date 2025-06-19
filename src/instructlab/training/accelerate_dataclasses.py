3ae765d71c475f361f0416bad4b9b463a98c6d2a
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2025-06-19 00:06:31 +0000

    Add debug activitities to 1.7 accelerate dataclasses script

--- src/instructlab/training/accelerate_dataclasses.py
+++ src/instructlab/training/accelerate_dataclasses.py
@@ -494,6 +494,7 @@ class ProfileKwargs(KwargsHandler):
     with_flops: bool = False
     with_modules: bool = False
     output_trace_dir: Optional[str] = None
+    debug_activities: Optional[List[DebugActivity]] = None 
 
     def _get_profiler_activity(self, activity: ProfilerActivity) -> torch.profiler.ProfilerActivity:
         """Get the profiler activity from the string.
@@ -542,6 +543,7 @@ class ProfileKwargs(KwargsHandler):
         return torch.profiler.profile(
             activities=activities,
             schedule=schedule,
+            debug_activities=self.debug_activities,
             on_trace_ready=self.on_trace_ready,
             record_shapes=self.record_shapes,
             profile_memory=self.profile_memory,
