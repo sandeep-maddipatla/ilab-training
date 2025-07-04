eecc4c28ebf12bb8ce03d53cfa33d27f2c76157e
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2025-07-03 19:44:02 -0500

    Save all changes

--- src/instructlab/training/multipack_sampler.py
+++ src/instructlab/training/multipack_sampler.py
@@ -59,7 +59,7 @@ def find_max_pack_work_with_padding(
     - The maximum batch length with padding for the given dataset.
     """
 
-    def get_effective_samples_per_minibatch(num_tokens_per_gpu):
+    def get_effective_samples_per_minibatch(work_per_gpu):
         """
         This nested function calculates the effective number of samples per minibatch for a given number of tokens per GPU.
 
@@ -77,7 +77,7 @@ def find_max_pack_work_with_padding(
             lengths = bucket_v(lengths)
 
         sampler = MultipackDistributedBatchSampler(
-            batch_max_length=num_tokens_per_gpu,
+            batch_max_work=work_per_gpu,
             lengths=lengths,
             num_replicas=torch.distributed.get_world_size(),
             rank=torch.distributed.get_rank(),
@@ -90,16 +90,16 @@ def find_max_pack_work_with_padding(
 
     samples_per_gpu = samples_per_minibatch / num_gpus
 
-    addition = int(avg_sample_len * 0.1 * samples_per_gpu)
-    packing_max_batch_len = int(avg_sample_len * samples_per_gpu)
+    addition = int(work_metric(avg_sample_len, multiplier=samples_per_gpu) * 0.1)
+    packing_max_batch_work = int(work_metric(avg_sample_len, multiplier=samples_per_gpu))
 
     avg_bs_per_minibatch = get_effective_samples_per_minibatch(
-        packing_max_batch_len + addition
+        packing_max_batch_work + addition
     )
     while avg_bs_per_minibatch <= samples_per_minibatch:
         addition *= 2
         avg_bs_per_minibatch = get_effective_samples_per_minibatch(
-            packing_max_batch_len + addition
+            packing_max_batch_work + addition
         )
 
     l = 0
@@ -107,7 +107,7 @@ def find_max_pack_work_with_padding(
     while r - l > 1:
         addition = (l + r) // 2
         avg_bs_per_minibatch = get_effective_samples_per_minibatch(
-            packing_max_batch_len + addition
+            packing_max_batch_work + addition
         )
 
         # check if simulation resulted in batch sizes close enough to goal and adjust if needed
@@ -120,7 +120,7 @@ def find_max_pack_work_with_padding(
         else:
             l = addition
 
-    return packing_max_batch_len + addition
+    return packing_max_batch_work + addition
 
 
 def find_packing_max_batch_work_and_grad_accum(
@@ -161,7 +161,7 @@ def find_packing_max_batch_work_and_grad_accum(
         grad_accum += 1
         samples_per_minibatch = effective_batch_size / grad_accum
         samples_per_gpu = samples_per_minibatch / num_gpus
-        if int(work_metric(avg_sample_len, samples_per_gpu)) < work_metric(dataset.get_lengths().max()):
+        if int(work_metric(avg_sample_len, multiplier=samples_per_gpu)) < work_metric(dataset.get_lengths().max()):
             raise RuntimeError(
                 f"Effective batch size is too low for multipack sampling, max sample length={dataset.get_lengths().max()} and min packing length={int(avg_sample_len * samples_per_gpu)}. "
                 "Switching to naive distributed sampling."
@@ -181,10 +181,10 @@ def find_packing_max_batch_work_and_grad_accum(
     return packing_max_batch_len, grad_accum
 
 def work_metric(sample_lengths, multiplier=None):
-    if not multiplier:
-        multiplier = len(sample_lengths)
     if not isinstance(sample_lengths, Iterable):
         sample_lengths = [sample_lengths]
+    if not multiplier:
+        multiplier = len(sample_lengths)
     return max(sample_lengths) * multiplier
 
 #@numba.njit
@@ -374,7 +374,7 @@ class MultipackDistributedBatchSampler(Sampler):
 
     def __init__(
         self,
-        batch_max_length: int,
+        batch_max_work: int,
         lengths: List[int],
         num_replicas: Optional[int] = None,
         rank: Optional[int] = None,
@@ -396,7 +396,7 @@ class MultipackDistributedBatchSampler(Sampler):
         self.rank = rank
         self.seed = seed
 
-        self.batch_max_length = batch_max_length
+        self.batch_max_work = batch_max_work
         self.lengths = lengths
         assert isinstance(self.lengths, np.ndarray)
 
@@ -419,13 +419,15 @@ class MultipackDistributedBatchSampler(Sampler):
         )
 
         # remove indices where the entries are longer than batch max length
-        indices = indices[self.lengths[indices] <= self.batch_max_length]
+        '''
+        indices = indices[work_metric(self.lengths[indices]) <= self.batch_max_work]
+        print(f'{len(indices)=}')
         if len(indices) < len(self.lengths):
             warnings.warn(
-                "Dropping %d samples longer than batch_max_length. Ensure that the right max_batch_length is used during data processing.",
+                "Dropping %d samples longer than batch_max_work. Ensure that the right max_batch_length is used during data processing.",
                 len(self.lengths) - len(indices),
             )
-
+        '''
         lengths = self.lengths[indices]
         works = [work_metric(ll, multiplier=1) for ll in lengths]
         works_cumsum = np.cumsum(works)
@@ -434,7 +436,7 @@ class MultipackDistributedBatchSampler(Sampler):
             lengths=lengths,
             works_cumsum=works_cumsum,
             rank=self.rank,
-            c=self.batch_max_length,
+            c=self.batch_max_work,
             n=self.num_replicas,
             padding=self.padding,
             enable_prints = self.enable_prints
