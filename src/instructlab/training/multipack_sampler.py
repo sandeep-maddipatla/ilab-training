4d306d1ac8cf35f88f6572add19d126e6c29eb99
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2025-07-08 00:42:22 +0000

    Configure metric_type through env variable

--- src/instructlab/training/multipack_sampler.py
+++ src/instructlab/training/multipack_sampler.py
@@ -36,6 +36,7 @@ import torch.distributed as dist
 
 from instructlab.training.hpu_utils import is_torch_hpu_available, bucket
 from collections.abc import Iterable
+import os
 
 
 def find_max_pack_work_with_padding(
@@ -186,12 +187,16 @@ def work_metric(sample_lengths, multiplier=None):
     if not multiplier:
         multiplier = len(sample_lengths)
     
-    metric_type = 'default'
+    metric_type = os.environ.get('WORK_METRIC_TYPE', 'default')
     wm = 0
     if metric_type == 'm2n':
         wm = max(sample_lengths) * max(sample_lengths) * multiplier
     elif metric_type == 'mlgm_n':
         wm = max(sample_lengths) * np.log2(max(sample_lengths)) * multiplier
+    elif metric_type == 'm_nlgn':
+        wm = max(sample_lengths) * multiplier * (np.log2(multiplier) if multiplier > 1 else 1)
+    elif metric_type == 'n2m':
+        wm = max(sample_lengths) * multiplier * multiplier
     else:
         # metric_type in ['mn', 'default']:
         wm = max(sample_lengths) * multiplier
