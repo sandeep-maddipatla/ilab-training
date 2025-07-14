b6ad6a044bb7e73ceb3f67003662440a97a68948
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2025-07-14 21:47:12 +0000

    Add epoch-result summary to parse.py

--- src/instructlab/training/parse.py
+++ src/instructlab/training/parse.py
@@ -1,6 +1,10 @@
 import re
 import sys
 
+def mmss_to_seconds(mmss_str):
+    minutes, seconds = map(int, mmss_str.split(":"))
+    return minutes * 60 + seconds
+
 losses = []
 s_it_values = []
 with open('result.log', 'r', encoding='utf-8') as f:
@@ -43,3 +47,29 @@ with open('result.log', 'r', encoding='utf-8') as f:
 print("num_loss_counted_tokens,num_tokens,micro_batch_size")
 for row in results:
     print(','.join(row))
+
+### Epoch Results
+# Open and read the log file
+print(f'Epoch Results:')
+pattern = re.compile(
+    r"Epoch (\d+): .*?(\d+)/\2 \[(\d+:\d+)<.*?,\s+([\d.]+)s/it\]"
+)
+result_dict = {}
+with open("result.log", "r") as file:
+    for line in file:
+        match = pattern.search(line)
+        if match:
+            epoch = int(match.group(1))
+            steps = int(match.group(2))
+            epoch_time = mmss_to_seconds(match.group(3))
+            seconds_per_it = float(match.group(4))
+            result_dict[epoch] = {
+                "epoch": epoch,
+                "steps": steps,
+                "seconds_per_it": seconds_per_it,
+                "epoch_time": epoch_time
+            }
+
+    for epoch in result_dict.keys():
+        r = result_dict[epoch]
+        print(f"Epoch: {epoch}, Steps: {r["steps"]}, Time: {r["epoch_time"]}, s/it: {r["seconds_per_it"]}")
