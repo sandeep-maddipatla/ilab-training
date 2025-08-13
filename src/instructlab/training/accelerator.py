c990b9a320003b171fd8591a81b3af944e422b7e
Sandeep Maddipatla <sandeep.maddipatla@intel.com>
2025-08-13 21:15:25 +0000

    Enable debug activities capture in profile, and do two steps by default

--- src/instructlab/training/accelerator.py
+++ src/instructlab/training/accelerator.py
@@ -80,9 +80,9 @@ class Accelerator:
         from accelerate import ProfileKwargs
 
         profile_kwargs = ProfileKwargs(
-            schedule_option={'wait':10, 'warmup':0, 'active':1, 'skip_first':0, 'repeat':1},
+            schedule_option={'wait':10, 'warmup':0, 'active':2, 'skip_first':0, 'repeat':1},
             activities=['cpu', 'hpu'],
-            #debug_activities=[DebugActivity.SYNAPSE_FUNCTION_CALLS, DebugActivity.BRIDGE_FUNCTION_CALLS],
+            debug_activities=[DebugActivity.SYNAPSE_FUNCTION_CALLS, DebugActivity.BRIDGE_FUNCTION_CALLS],
             on_trace_ready=torch.profiler.tensorboard_trace_handler('./profile_logs'),
             profile_memory=True,
             record_shapes=False   
